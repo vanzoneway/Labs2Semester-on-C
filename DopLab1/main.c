90fd68525c32223fbf02c83a53ac6a61ec656a15
vanz_oneway <vanz.evergarden0@gmail.com>
2023-02-23 00:14:30 +0300

    changed main.c file from vanz_oneway branch

--- DopLab1/main.c
+++ DopLab1/main.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    FILE *fp;
-    char path[1035];
-
-    fp = popen("DEL need_to_remove.txt", "r"); // выполнить команду "dir" и открыть поток для чтения ее вывода
-    if (fp == NULL) {
-        printf("Ошибка выполнения команды\n");
-        return 1;
-    }
-
-    while (fgets(path, sizeof(path)-1, fp) != NULL) { // читать вывод команды построчно
-        printf("%s", path);
-    }
-
-    pclose(fp); // закрыть поток
-    return 0;
+    printf("Hello, vanz_oneway");
 }
 
 
