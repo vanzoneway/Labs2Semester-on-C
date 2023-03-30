6e25b396f6eb077f23df17f4e9ca63ba0228096f
vanz_oneway <vanz.evergarden0@gmail.com>
2023-03-30 18:52:06 +0300

    added 4 and 5th lab from 2 semester

---
+++ Lab4/menu.h
@@ -0,0 +1,8 @@
+//
+// Created by PC on 19.03.2023.
+//
+
+#ifndef LAB4_MENU_H
+#define LAB4_MENU_H
+
+#endif //LAB4_MENU_H
---
+++ Lab4/task.h
@@ -0,0 +1,8 @@
+//
+// Created by PC on 19.03.2023.
+//
+
+#ifndef LAB4_TASK_H
+#define LAB4_TASK_H
+
+#endif //LAB4_TASK_H
